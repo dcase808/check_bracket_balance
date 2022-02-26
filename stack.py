4d2c975eaca5c848fc40ede50a3099a83833c202
case <56609440+dcase808@users.noreply.github.com>
2022-02-27 00:54:58 +0100

    Update stack.py

--- stack.py
+++ stack.py
@@ -1,1 +1,16 @@
+class Stack:
+    def __init__(self):
+        self.items = []
+    
+    def push(self, item):
+        return self.items.append(item)
+    
+    def pop(self):
+        return self.items.pop()
+    
+    def is_empty(self):
+        return True if self.items == [] or self.items == None else False
 
+    def peek(self):
+        if not self.is_empty():
+            return self.items[-1]
